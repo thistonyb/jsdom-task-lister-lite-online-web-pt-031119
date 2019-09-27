2836919f746734d236f159fc419a5c14d79f8213
thistonyb <this.tonybaker@gmail.com>
2019-09-26 18:01:25 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", addItem);
 });
+
+function addItem(event) {
+  event.preventDefault();
+  let newInput = document.getElementById("new-task-description");
+  let listItem = document.createElement("li");
+  let ulTasks = document.getElementById("tasks");
+  listItem.innerHTML = newInput.value;
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "X";
+  deleteButton.addEventListener("click", onDeleteClick);
+  listItem.appendChild(deleteButton);
+
+  ulTasks.appendChild(listItem);
+  newInput.value = "";
+}
+
+function onDeleteClick(event) {
+  const deleteButton = event.currentTarget;
+  const listItem = deleteButton.parentElement;
+  const list = listItem.parentElement;
+  list.removeChild(listItem);
+}
